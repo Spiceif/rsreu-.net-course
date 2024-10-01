a8280930014ccd4aa1462bcd0533c497071cb66a
Spiceif <asif.aliev0705@gmail.com>
2024-10-01 22:50:07 +0300

    Выполнена лабораторная работа под 4 вариантом

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,18 @@
+﻿//var 4
+
+namespace Lab01
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Введите n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double result;
+            result = Math.Pow(-1, n) * ((Math.Pow(n, 2) + 1) / (Math.Pow(n, 3) + 3));
+            Console.WriteLine("Результат: ");
+            Console.WriteLine(result);
+        }
+    }
+}
+
